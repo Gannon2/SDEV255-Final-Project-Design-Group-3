69f9d70ef0025a34972e380cb097cf8655004136
Tigerbear123 <131217726+Tigerbear123@users.noreply.github.com>
2023-12-15 18:31:46 -0500

    Update login.js

--- login.js
+++ login.js
@@ -13,7 +13,8 @@ mongoose.connect('mongodb://localhost/myapp', { useNewUrlParser: true, useUnifie
 // Create a user schema
 const userSchema = new mongoose.Schema({
   username: String,
-  password: String
+  password: String, 
+  role: String //This is to differentiate student/teacher
 });
 
 // Create a user model
@@ -21,8 +22,8 @@ const User = mongoose.model('User', userSchema);
 
 // Create a registration route
 app.post('/register', (req, res) => {
-  const { username, password } = req.body;
-  const user = new User({ username, password });
+  const { username, password, role } = req.body;
+  const user = new User({ username, password, role });
   user.save()
     .then(() => res.send('User created successfully'))
     .catch(err => console.log(err));
@@ -34,6 +35,7 @@ app.post('/login', (req, res) => {
   User.findOne({ username, password })
     .then(user => {
       if (user) {
+        req.session.role = user.role;
         res.send('Login successful');
       } else {
         res.send('Invalid credentials');
@@ -42,7 +44,22 @@ app.post('/login', (req, res) => {
     .catch(err => console.log(err));
 });
 
-// Create a middleware function
+// Create a middleware function to check the user's role
+const checkRole = (role) => {
+  return (req, res, next) => {
+    // get the user's role from the session
+    const userRole = req.session.role;
+    if (userRole === role) {
+      // if the user has the required role, proceed to the next middleware
+      next();
+    } else {
+      // otherwise, send an unauthorized response
+      res.status(401).send('Unauthorized');
+    }
+  };
+};
+
+// Create a middleware function (this might be the same up above)
 const authenticate = (req, res, next) => {
   const { username, password } = req.body;
   if (username === 'admin' && password === 'password') {
@@ -51,6 +68,18 @@ const authenticate = (req, res, next) => {
     res.send('Invalid credentials');
   }
 };
+
+// Create a student view
+app.get('/student', checkRole('student'), (req, res) => {
+  // render the student view
+  res.send('Student view');
+});
+
+// Create a teacher view
+app.get('/teacher', checkRole('teacher'), (req, res) => {
+  // render the teacher view
+  res.send('Teacher view');
+});
 
 // Protect your routes
 app.get('/protected', authenticate, (req, res) => {
