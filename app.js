7d408a415842983e307bafef03e67e449fd59821
Gannon2 <111807284+Gannon2@users.noreply.github.com>
2023-12-16 02:00:57 +0000

    Co-authored-by: Kesny R <Kesny888@users.noreply.github.com> Co-authored-by: Aung Zin Paing <apaing09@users.noreply.github.com> Co-authored-by: Tigerbear123 <Tigerbear123@users.noreply.github.com>

--- app.js
+++ app.js
@@ -39,7 +39,11 @@ app.get('/', (req, res) => res.render('home', { title: 'Home' }));
 app.use(authRoutes);
 app.get('/home', (req, res) => res.render('home', { title: 'Home' }));
 app.use('/courses', requireAuth, courseRoutes);
-
+app.get('/teacher', (req, res) => res.render ('teacher', { title: 'Teacher Name'}))
+app.use('/teacher/add-course', (req, res) => {
+    const { courseName, courseCode } = req.body;
+    res.redirect('/teacher');
+});
 // 404 page
 app.use((req, res, next) => { 
     res.status(404).redirect('Error', { title: 'Error 404', err: '404' });
