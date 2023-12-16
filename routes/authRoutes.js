ac3245e39cf85d702af1138085018356caafced1
Gannon2 <111807284+Gannon2@users.noreply.github.com>
2023-12-16 01:53:26 +0000

    Co-authored-by: Aung Zin Paing <apaing09@users.noreply.github.com> Co-authored-by: Tigerbear123 <Tigerbear123@users.noreply.github.com> Co-authored-by: Kesny R <Kesny888@users.noreply.github.com>

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const authController = require('../controllers/authController')
+const authController = require('../controllers/authController');
 const router = Router();
 
 router.get('/signup', authController.signup_get);
